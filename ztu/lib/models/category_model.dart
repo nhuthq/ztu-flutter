d2132d5058626141a45fd003df8d010228ce56b7
nhuthq <>
2023-04-28 10:57:14 +0700

    add model

---
+++ ztu/lib/models/category_model.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+
+class Category extends Equatable {
+  final String name;
+  final String imgUrl;
+
+  Category ({
+    required this.name,
+    required this.imgUrl
+  })
+
+
+  @override
+  List<Object?> get props => [name, imgUrl];
+  
+  static List<Category> categories = [
+    Category(
+      name: 'Soft Drink',
+      imgUrl: 'https://images.unsplash.com/photo-1606168094336-48f205276929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    ),
+    Category(
+      name: 'Soda',
+      imgUrl: 'https://images.unsplash.com/photo-1567671899076-51a64ddb7c5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1024&q=80',
+    ),
+    Category(
+      name: 'Juice',
+      imgUrl: 'https://images.unsplash.com/photo-1613478223719-2ab802602423?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    ),
+    Category(
+      name: 'Milkshake',
+      imgUrl: 'https://images.unsplash.com/photo-1543573852-1a71a6ce19bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    ),
+    Category(
+      name: 'Coffee',
+      imgUrl: 'https://images.unsplash.com/photo-1461023058943-07fcbe16d735?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+    ),
+    Category(
+      name: 'Beer',
+      imgUrl: 'https://images.unsplash.com/photo-1608270586620-248524c67de9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    ),
+    Category(
+      name: 'Cooktail',
+      imgUrl: 'https://images.unsplash.com/photo-1542849187-5ec6ea5e6a27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80',
+    ),
+  ];
+}
