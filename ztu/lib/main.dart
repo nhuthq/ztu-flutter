f3cf7b4af2bb0fe1b735a08658f45080d261ec77
nhuthq <>
2023-05-16 13:54:48 +0700

    add Firebase

--- ztu/lib/main.dart
+++ ztu/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ztu/blocs/cart/cart_bloc.dart';
@@ -9,7 +10,9 @@ import 'package:ztu/config/theme.dart';
 import 'package:ztu/screens/home/home_screen.dart';
 import 'package:ztu/screens/splash/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
