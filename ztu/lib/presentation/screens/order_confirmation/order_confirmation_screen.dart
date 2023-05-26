6e3395110ab5a8d9f667ebb0d4a7f149c8eef1c1
nhuthq <>
2023-05-26 11:57:30 +0700

    add assets

--- ztu/lib/presentation/screens/order_confirmation/order_confirmation_screen.dart
+++ ztu/lib/presentation/screens/order_confirmation/order_confirmation_screen.dart
@@ -1,5 +1,6 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:ztu/presentation/widgets/custom_appbar.dart';
+import 'package:ztu/presentation/widgets/custom_nav_bar.dart';
 
 class OrderConfirmationScreen extends StatelessWidget {
   static const String routeName = '/order_confirmation';
@@ -13,36 +14,27 @@ class OrderConfirmationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Timer(
-        const Duration(seconds: 2),
-        () => Navigator.of(context)
-            .pushNamedAndRemoveUntil('/home', (route) => false));
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Image.asset(
-              'assets/images/logo.png',
-              width: 125,
-              height: 125,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            color: Colors.black,
-            child: Text(
-              'Zero to Unicorn',
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall!
-                  .copyWith(color: Colors.white),
-            ),
-          )
-        ],
+      appBar: const CustomAppBar(title: 'Order Confirmation'),
+      bottomNavigationBar: const CustomBottomNavBar(screen: routeName),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  color: Colors.black,
+                ),
+                // Positioned(
+                //   child: 
+                // )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
